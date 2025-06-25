1526007110f451445066b46b8485ddb82427d4e0
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-06-25 06:14:21 +0300

    Worked on socket1.py.

---
+++ socket1.py
@@ -0,0 +1,12 @@
+import socket
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as my_socket:
+    my_socket.connect(("data.pr4e.org", 80))
+    cmd = "GET /romeo.txt HTTP/1.1\r\nHost: data.pr4e.org\r\n\r\n".encode()
+    my_socket.send(cmd)
+
+    while True:
+        data = my_socket.recv(1024)
+        if len(data) < 1:
+            break
+        print(data.decode())
