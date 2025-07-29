c958805398a129561e2f8a8a0d1dd997043d3205
Abel Kevin Kipkosgei <abelkevinkipkosgei@gmail.com>
2025-07-29 04:22:12 +0300

    Worked on gline.js.

---
+++ gline.js
@@ -0,0 +1,4 @@
+gline = [ ['Month','umich.edu','indiana.edu','ufp.pt','ucdavis.edu','uct.ac.za','berkeley.edu','columbia.edu','gmail.com','mac.com','virginia.edu'],
+['2005-12',57,12,10,11,14,12,13,10,12,9],
+['2006-01',76,25,27,21,18,17,14,13,10,13]
+];
